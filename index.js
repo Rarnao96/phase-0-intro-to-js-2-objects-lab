506a0218ba300320ac3d2e4b2fb249bd1f712cf6
Rachel Arnao <rachel.e.arnao@gmail.com>
2023-03-06 00:09:21 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+    const employee = {
+        name: "value1",
+        streetAddress: "value2",
+    };
+
+    function updateEmployeeWithKeyAndValue(obj, key, value){
+        const newEmployee = {...employee };
+        return {
+            ...employee,
+            ["name"]: "Sam",
+            ["streetAddress"]: "11 Broadway",
+        }
+    }
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee["streetAddress"] = '12 Broadway';
+        return employee;
+    }
+    
+    function deleteFromEmployeeByKey(employee, key){
+        const newEmployee = {...employee };
+        delete newEmployee.name;
+        return newEmployee;
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(employee, key){
+        delete employee.name;
+        return employee;
+    }
+
